880c46560584c165f6ed5274fe2c11127076fdb4
Scott Rabin <scottrabin@gmail.com>
2018-03-07 19:28:51 -0800

    Add React to the electron

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Application extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <h1>Hello from React!</h1>
+        );
+    }
+}
